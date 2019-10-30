6df18c98bff2469eb0e730dffe1dfcc398666ab1
Ben O'Harra <Ben@Kallies-MacBook-Pro.local>
2019-10-30 14:18:42 -0700

    Update router to top level app component

--- app/src/app/app.tsx
+++ app/src/app/app.tsx
@@ -7,13 +7,25 @@ import {
   FunctionComponent,
   Fragment} from 'react';
 
+import {
+    BrowserRouter as Router,
+    Switch, Route
+} from 'react-router-dom';
+
 import { HomePage } from './home/home-page';
+import { Industry } from './sector/industry';
+
+
 
 export const App: FunctionComponent = function() {
 
   return (
-    <Fragment>
-      <HomePage />
-    </Fragment>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage}/>  
+        <Route path="/industry" component={Industry}/>
+      </Switch>
+    </Router>
+  
   );
 };
